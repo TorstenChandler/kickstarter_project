496bbcd9c44bfb1f907e579b1e106ad045c005c9
Olmo <Olmo@MacBook-Pro-2.local>
2024-05-17 16:34:36 +0200

    upating model

--- utils/models.py
+++ utils/models.py
@@ -2,14 +2,17 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
 
 """
 @TODO:
-
     1) CREATE NEW COLUMN FUNDED FROM (status) Live?
     2) NEW COLUMNS DURATION, MONTH, YEAR FROM START AND END
     3) SCALING UNCLEAR
 """
+
+funded = FunctionTransformer(lambda row: True if row.pledged > row.goal else False)
+
 
 logistical_regression = Pipeline([
     (
@@ -32,3 +35,5 @@ random_forest = Pipeline([
     ("logreg", RandomForestClassifier())
     ]
 )
+
+__all__ = [logistical_regression,random_forest]
