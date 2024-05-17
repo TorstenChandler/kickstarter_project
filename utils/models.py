f304299f1f4c21608b0d8a738c7d148f270657eb
Olmo <Olmo@MacBook-Pro-2.local>
2024-05-17 16:30:03 +0200

    adding eda

--- utils/models.py
+++ utils/models.py
@@ -3,6 +3,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
+"""
+@TODO:
+
+    1) CREATE NEW COLUMN FUNDED FROM (status) Live?
+    2) NEW COLUMNS DURATION, MONTH, YEAR FROM START AND END
+    3) SCALING UNCLEAR
+"""
+
 logistical_regression = Pipeline([
     (
         "preprocess", 
