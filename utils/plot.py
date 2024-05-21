5e694d7f8ff595d41d606fc4c38dea19f5ffc432
MonaliTalele <monali.talele@gmail.com>
2024-05-21 10:40:15 +0200

    some corrections and added countplot, adding my EDA file

--- utils/plot.py
+++ utils/plot.py
@@ -12,12 +12,14 @@ __all__ = ["histogram","scatter","pair"]
 
 # RC Params for seaborn
 plt.rcParams['figure.figsize'] = FIG_SIZE  # Set default figure size
-plt.rcParams['font.family'] = 'serif'    # Set default font family
+#plt.rcParams['font.family'] = 'calibri'    # Set default font family
 plt.rcParams['font.size'] = FONT_SIZE          # Set default font size
 plt.rcParams['lines.linewidth'] = 2      # Set default line width
 #plt.rcParams['style'] == plt_style
 
-def histogram(data, x, xtitle):
+def histogram(data, x, xtitle=None):
+    if xtitle is None:
+        xtitle = x
     fig, ax = plt.subplots()
     #plt_style
     sns.histplot(data = data, x=x, ax=ax)
@@ -26,19 +28,34 @@ def histogram(data, x, xtitle):
     ax.set_ylabel('Frequency')
     plt.show()
     
-def scatter(data ,x, y, hue,xtitle="x", ytitle="y",):
-    if xtitle == "x":
+def scatter(data ,x, y, hue=None, xtitle=None, ytitle=None):
+    if xtitle is None:
         xtitle = x
-    if ytitle == "y":
+    if ytitle is None:
         ytitle = y
     fig, ax = plt.subplots()
-    #plt_style
-    sns.scatterplot(data = data, x = x, y = y, hue = hue, palette=CMAP_RAINBOW, ax= ax)
+    if hue is None:
+        print("Hue is : None ", hue)
+        sns.scatterplot(data = data, x = x, y = y, ax= ax)
+    else: 
+        print("Hue is : not None ", hue)
+        sns.scatterplot(data = data, x = x, y = y, hue = hue,palette=CMAP_RAINBOW, ax= ax)
     #(lambda hue=hue: {'hue': hue, 'palette': 'rainbow'} if hue else {})(hue)
     ax.set_title(f"Plot of {xtitle} vs {ytitle}")
     ax.set_xlabel(xtitle)
     ax.set_ylabel(ytitle)
     plt.show()
+
+def countplot(data, x, xtitle=None):
+    if xtitle is None:
+        xtitle = x
+    fig, ax = plt.subplots()
+    sns.countplot(x=x, data=data, palette=CMAP_RAINBOW, order = data[x].value_counts().index)
+    # Rotate the x labels by 90 degrees and hide the extra text
+    _ = plt.xticks(rotation=90)
+    ax.set_title(f"Count of {xtitle}")
+    ax.set_xlabel(xtitle)
+    ax.set_ylabel("Count")
 
 def pair():
     pass 
