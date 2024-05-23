6b7668d0516ec90a837c3c6a9b8fff5f65f0a0dc
OlmoDalco <yannik.hake@gmail.com>
2024-05-23 17:03:02 +0200

    updating models

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np 
-from utils.model import get_valid_catoricals
-
-def predict(project : pd.DataFrame):
-    return np.random.uniform(0, 1)
+import joblib
+from utils.model import get_valid_catoricals, clean_data
+model = joblib.load('./utils/pickles/ensemble.joblib')
+def predict(project)->tuple:
+    data = clean_data(pd.DataFrame(project))
+    results = model.predict_proba(data)
+    failure = results[0][0]
+    success = results[0][1]
+    return (failure,success)
 
 def main():
     st.title("Enter Kickstarter Project Details")
@@ -12,8 +17,9 @@ def main():
     available_categories, available_subcategories, available_countries = get_valid_catoricals()
 
     # User input fields
-    user = st.selectbox("I am an :", ['Entrepreneur', 'Investor'], index=0)
+    #user = st.selectbox("I am an :", ['Entrepreneur', 'Investor'], index=0)
     name = st.text_input("Enter Kickstarter Project Name:", value = "Awesome Project")
+    goal = st.number_input("Enter Project Goal in $:", value=10000, step=1000)
     country = st.selectbox("Select Project Country:", available_countries, index=0)
 
     # Create two columns for category and subcategory
@@ -29,29 +35,32 @@ def main():
         launch_date = st.date_input("Enter Project Launch Date:")
     with col_deadline:
         deadline_date = st.date_input("Enter Project Deadline:")
-    goal = st.number_input("Enter Project Goal in $:", value=10000, step=1000)
+    backers = st.number_input("Expected backers:", value=100)
 
+    
     # Create a dictionary to store the input data
     data = {
-        "Project Name": [name],
+        "Name": [name],
         "Category": [category],
-        "Sub-Category": [subcategory],
+        "Subcategory": [subcategory],
         "Country": [country],
-        "Launch Date": [launch_date],
-        "Deadline Date": [deadline_date],
-        "Project Goal ($)": [goal]
+        "Launched": [launch_date],
+        "Deadline": [deadline_date],
+        "Pledged": 0,
+        "Goal": [goal],
+        "Backers": backers
     }
 
     # Convert the dictionary to a pandas DataFrame
-    input = pd.DataFrame(data)
+    
 
     # Display the result
-    if st.button("Show Result"):
-        result = predict(input)
-        if result >= 0.5:
-            st.success(f"Project '{name}' has a {round(result*100)}% probability of Success!")
+    if st.button("Predict Outcome"):
+        failure, success = predict(data)
+        if success >= 0.5:
+            st.success(f"Project '{name}' has a {round(success*100)}% probability of Success!")
         else:
-            st.error(f"Project '{name}' is likely to be unsuccessful. Consider revising the project or selecting another one.")
+            st.error(f"Project '{name}' has a high probability of {round(failure*100)}% to fail. Consider revising the project or selecting another one.")
     #st.success(f"Name: {name}, Category: {category}, Sub-Category: {subcategory}, Country: {country}, Launch Date: {launch_date}, Deadline: {deadline_date}, Goal: {goal} ")
 
 if __name__ == "__main__":
