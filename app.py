a5e1af155d2fbea21fe12b401072632892755291
Olmo <Olmo@MacBook-Pro-2.local>
2024-05-24 11:35:28 +0200

    updating model in app

--- app.py
+++ app.py
@@ -3,39 +3,41 @@ import pandas as pd
 import numpy as np 
 import joblib
 from utils.model import get_valid_catoricals, clean_data
-model = joblib.load('./utils/pickles/ensemble.joblib')
+import datetime
+model = joblib.load('./utils/pickles/xgboost.joblib')
 def predict(project)->tuple:
     data = clean_data(pd.DataFrame(project))
     results = model.predict_proba(data)
     failure = results[0][0]
     success = results[0][1]
     return (failure,success)
+available_categories, available_subcategories, available_countries = get_valid_catoricals()
 
 def main():
     st.title("Enter Kickstarter Project Details")
 
-    available_categories, available_subcategories, available_countries = get_valid_catoricals()
+    
 
     # User input fields
     #user = st.selectbox("I am an :", ['Entrepreneur', 'Investor'], index=0)
-    name = st.text_input("Enter Kickstarter Project Name:", value = "Awesome Project")
-    goal = st.number_input("Enter Project Goal in $:", value=10000, step=1000)
+    name = st.text_input("Enter Kickstarter Project Name:", value = "Most Comfortable Insoles on Earth")
+    goal = st.number_input("Enter Project Goal in $:", value=7500, step=100)
     country = st.selectbox("Select Project Country:", available_countries, index=0)
 
     # Create two columns for category and subcategory
     col_cat, col_subcat = st.columns(2)
     with col_cat:
-        category = st.selectbox("Select Project Category:", available_categories, index=0)
+        category = st.selectbox("Select Project Category:", available_categories, index=available_categories.index("Fashion"))
     with col_subcat:
-        subcategory = st.selectbox("Select Project Sub-Category:", available_subcategories, index=0)
+        subcategory = st.selectbox("Select Project Sub-Category:",available_subcategories, index=available_subcategories.index("Footwear"))
     
     # Create two columns for launch_date and deadline_date
     col_launch, col_deadline = st.columns(2)
     with col_launch:
-        launch_date = st.date_input("Enter Project Launch Date:")
+        launch_date = st.date_input("Enter Project Launch Date:",datetime.date(2024, 5, 15) )
     with col_deadline:
-        deadline_date = st.date_input("Enter Project Deadline:")
-    backers = st.number_input("Expected backers:", value=100)
+        deadline_date = st.date_input("Enter Project Deadline:", datetime.date(2024, 6, 6) )
+    backers = st.number_input("Expected backers:", value=184)
 
     
     # Create a dictionary to store the input data
